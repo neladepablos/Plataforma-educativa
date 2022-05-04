e960feb11402706a6bae6a4b1314986c070dcb60
Marianela Ruiz <marianelaruizdepablos@gmail.com>
2022-05-03 20:38:48 -0500

    Agrega Provider a nuestra app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import store from './redux/store';
+import { Provider } from 'react-redux';
+import { getAllSpecialities } from './redux/actionCreators';
+import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 import "./styles/styles.scss";
+
+
+store.dispatch(getAllSpecialities());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>      
+        <App />    
+    </Provider>
+    
   </React.StrictMode>
 );
 
